ac74577cd63a1f24c4158daa533f694c2e30e4bf
PantherBruv <grifithsjack@gmail.com>
2023-01-30 23:49:58 +1100

    Update config.lua
    Adds QBCore warning lmao

--- shared/config.lua
+++ shared/config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
 Config.AcePermName = 'command'
-Config.Sendnotif = true -- Only ox_lib notification is supported currently!
+Config.Sendnotif = true -- Only ox_lib notification is supported currently! - Requires QBCore to pull all players so only use if you're using QBCore Framework
